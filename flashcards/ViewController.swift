fca1999e1a52a889fb48af29fd5850ab49ec115c
shayan <42shayan42@gmail.com>
2021-02-21 11:09:07 -0800

    fixed it

--- flashcards/ViewController.swift
+++ flashcards/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var answer: UILabel!
+    
+    @IBOutlet weak var question: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didtap(_ sender: Any) {
+        
+        question.isHidden = true
+        
     }
 
 
